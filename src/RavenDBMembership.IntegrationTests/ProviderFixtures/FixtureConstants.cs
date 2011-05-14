a7a0338d940148c09cdcb785803d948f479588cc
Wilby C. Jackson Jr <jacksw02@gmail.com>
2011-05-14 07:28:19 -0400

    Removing all work to replace with work I have done. This is not because the current works is bad, only because I already did the work in a much differnt fashion and with different tools continued

--- src/RavenDBMembership.IntegrationTests/ProviderFixtures/FixtureConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RavenDBMembership.IntegrationTests.ProviderFixtures
-{
-    public class FixtureConstants
-    {
-        public const string DatabaseName = "RavenDBMembershipTestSqlDatabase";
-        public const string NameOfConfiguredMembershipProvider = "UsedAsDefaultConfigurationInIntegrationTests_TypeIsIgnored";
-    }
-}
