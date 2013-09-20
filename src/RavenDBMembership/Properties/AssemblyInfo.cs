5795deddced1b1ce5ee26aa7cec34a3d9fe6d7a4
Wilby Jackson <jacksw02@gmail.com>
2013-09-19 21:19:43 -0400

    Last commit with v2.0.2261 Raven Client, Upadating to 2.5.2700

--- src/RavenDBMembership/Properties/AssemblyInfo.cs
+++ src/RavenDBMembership/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RavenDBMembership")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Asp.Net Membership and Role Provider for RavenDB")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Wilby C. Jackson Jr.")]
 [assembly: AssemblyProduct("RavenDBMembership")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCopyright("Copyright © Wilby C. Jackson Jr. 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.2261.0")]
+[assembly: AssemblyFileVersion("2.0.2261.0")]
